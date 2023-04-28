ffed275ac503f0308cd7ccb45fc7c9ed6e8edec0
estephaniecarmona <estephflcl@gmail.com>
2023-04-28 16:19:11 -0500

    added colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
 
-function Square({value, onSquareClick}) {  
+function Square({value, onSquareClick, color}) {  
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className="square" onClick={onSquareClick} style={{backgroundColor: color}}>
       {value}
     </button>
   );
@@ -35,20 +35,21 @@ function Board({ xIsNext, squares, onPlay }){
     <React.Fragment>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+        <Square value={squares[0]} color={"yellow"} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} color={"blue"} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} color={"green"} onSquareClick={() => handleClick(2)}/>
+        <Square value={squares[3]} color={"red"} onSquareClick={() => handleClick(3)}/>
       </div>
-      <div className="board-row">
+      {/* <div className="board-row">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
         <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
         <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
+      </div> */}
+      {/* <div className="board-row">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
         <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
+      </div> */}
     </React.Fragment>
   );
 
