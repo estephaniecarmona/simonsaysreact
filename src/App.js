986c56dcb272510a6c5be471992457ef81ac3f6e
estephaniecarmona <estephflcl@gmail.com>
2023-05-01 13:19:45 -0500

    get random color to generate

--- src/App.js
+++ src/App.js
@@ -1,95 +1,90 @@
 import React from 'react';
 import { useState } from 'react';
 
-function Square({value, onSquareClick, color}) {  
+
+
+function Square({ onSquareClick, color }) {
   return (
-    <button className="square" onClick={onSquareClick} style={{backgroundColor: color}}>
-      {value}
+    <button className="square" onClick={onSquareClick} style={{ backgroundColor: color }}>
+
     </button>
   );
 }
 
-function Board({ xIsNext, squares, onPlay }){
+function randomInt(number) {
+  return Math.floor(Math.random() * (number))
+}
+
+function randomPick(array) {
+  return array[randomInt(array.length)]
+}
+
+function Board({ squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
+    
     onPlay(nextSquares)
   }
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-      status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
-  
-  
+
+  let status = "";
+
+  const componentArray = [
+    <Square color={"yellow"} onSquareClick={() => handleClick(0)} />,
+    <Square color={"blue"} onSquareClick={() => handleClick(1)} />,
+    <Square color={"green"} onSquareClick={() => handleClick(2)} />,
+    <Square color={"red"} onSquareClick={() => handleClick(3)} />,
+  ]
+
+  randomPick(componentArray)
+
   return (
     <React.Fragment>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} color={"yellow"} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} color={"blue"} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} color={"green"} onSquareClick={() => handleClick(2)}/>
-        <Square value={squares[3]} color={"red"} onSquareClick={() => handleClick(3)}/>
+        {componentArray}
       </div>
-      {/* <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div> */}
-      {/* <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div> */}
     </React.Fragment>
   );
-
-  }
+}
 
 
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];  
-  
+  const [history] = useState([Array(9).fill(null)]);
+
+  const colors = ["yellow", "blue", "green", "red"]
+
+
   function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
+
   }
 
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
+  function startGame() {
+    console.log(randomPick(colors))
   }
+
+
 
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
       description = 'Go to move #' + move;
     } else {
-      description = 'Go to game start';
+      description = 'PLAY';
     }
     return (
       <li>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        <button onClick={() => startGame()}>{description}</button>
       </li>
     );
-  }); 
+  });
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
